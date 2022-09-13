e1e70787fe828293cb3bf6d53bb5a922348b5014
Phil Metscher <philmetschermailt@gmx.de>
2022-09-13 15:32:35 +0200

    added cards to index.js

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+import { createCard } from "./components/card/card.js";
+
+const cardContainer = document.querySelector('[data-js="card-list"]');
+
+const cards = [
+  {
+    question:
+      "In the Kingdom Heart series who provides the english voice for Master Eraqus?",
+    answer: "Mark Hamill",
+    tags: "kingdomheart,mastereraqus,english voices",
+  },
+  {
+    question:
+      "Daniel Radcliffe became a global star in the film industry due to his performance in which film franchise?",
+    answer: "Harry Potter",
+    tags: "danielradcliffe,hewhomustnotbenamed,abracadabra",
+  },
+  {
+    question:
+      'Which former US president was nicknamed "Teddy" after he refused to shoot a defenseless black bear?',
+    answer: "Theodore Roosevelt",
+    tags: "usa,president,teddybear",
+  },
+];
+
+cards.forEach((card) => {
+  const newCard = createCard(card.question, card.answer, card.tags);
+  cardContainer.append(newCard);
+});
+
 const answerButton = document.querySelector('[data-js="answer-button"]');
 const answer = document.querySelector('[data-js="card-answer"]');
 const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
